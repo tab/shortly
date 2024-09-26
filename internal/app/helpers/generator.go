80d5bfca0a7ac8ca66709d6b094425102d6beaa0
tab <tukhfatullin@gmail.com>
2024-09-26 22:11:06 +0300

    feat(http-server): add HTTP server and initial endpoints

---
+++ internal/app/helpers/generator.go
@@ -0,0 +1,17 @@
+package helpers
+
+import (
+	"crypto/rand"
+)
+
+func GenerateShortID() string {
+	const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	bytes := make([]byte, 8)
+	if _, err := rand.Read(bytes); err != nil {
+		return "randomID"
+	}
+	for i, b := range bytes {
+		bytes[i] = chars[b%byte(len(chars))]
+	}
+	return string(bytes)
+}
---
+++ internal/app/server/server.go
@@ -0,0 +1,17 @@
+package server
+
+import (
+	"net/http"
+)
+
+func Run() error {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", HandleCreateShortLink)
+	mux.HandleFunc("/{id}", HandleGetShortLink)
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+	return nil
+}

3f4a1413eaee150c25528523c5abae4d9b6d909a
tab <tukhfatullin@gmail.com>
2024-10-03 21:05:32 +0300

    feat(config): allow configuration via env, flags, or use defaults

--- internal/app/config/config_test.go
+++ internal/app/config/config_test.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,44 +12,47 @@ func TestInitConfig(t *testing.T) {
 	tests := []struct {
 		name     string
 		args     []string
+		env      map[string]string
 		expected Options
 	}{
 		{
-			name: "Default flags",
+			name: "Use default values",
 			args: []string{},
+			env:  map[string]string{},
 			expected: Options{
 				Addr:    "localhost:8080",
 				BaseURL: "http://localhost:8080",
 			},
 		},
+		{
+			name: "Use env vars",
+			args: []string{"-a", "localhost:5000", "-b", "http://localhost:5000"},
+			env: map[string]string{
+				"SERVER_ADDRESS": "localhost:3000",
+				"BASE_URL":       "http://localhost:3000",
+			},
+			expected: Options{
+				Addr:    "localhost:3000",
+				BaseURL: "http://localhost:3000",
+			},
+		},
 		//{
-		//	name: "Custom address",
-		//	args: []string{"-a", "0.0.0.0:9090"},
+		//	name: "Use flags",
+		//	args: []string{"-a", "localhost:4000", "-b", "http://localhost:4000"},
+		//	env:  map[string]string{},
 		//	expected: Options{
-		//		Addr:    "0.0.0.0:9090",
-		//		BaseURL: "http://localhost:8080",
-		//	},
-		//},
-		//{
-		//	name: "Custom base URL",
-		//	args: []string{"-b", "http://example.com"},
-		//	expected: Options{
-		//		Addr:    "localhost:8080",
-		//		BaseURL: "http://example.com",
-		//	},
-		//},
-		//{
-		//	name: "Custom address and base URL",
-		//	args: []string{"-a", "0.0.0.0:8081", "-b", "http://example.com"},
-		//	expected: Options{
-		//		Addr:    "0.0.0.0:8081",
-		//		BaseURL: "http://example.com",
+		//		Addr:    "localhost:4000",
+		//		BaseURL: "http://localhost:4000",
 		//	},
 		//},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
+			for key, value := range test.env {
+				os.Setenv(key, value)
+			}
+
 			flag.CommandLine = flag.NewFlagSet(test.name, flag.ContinueOnError)
 			flag.CommandLine.Parse(test.args)
 
@@ -56,6 +60,10 @@ func TestInitConfig(t *testing.T) {
 
 			assert.Equal(t, test.expected.Addr, result.Addr)
 			assert.Equal(t, test.expected.BaseURL, result.BaseURL)
+
+			for key := range test.env {
+				os.Unsetenv(key)
+			}
 		})
 	}
 }

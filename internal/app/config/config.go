275b0a105bc91affd41bbe8ff6862924ea804a1f
tab <tukhfatullin@gmail.com>
2024-10-03 21:05:32 +0300

    Use env or flags or defaults

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -2,19 +2,49 @@ package config
 
 import (
 	"flag"
+	"os"
 )
+
+const ServerAddress = "localhost:8080"
+const BaseURL = "http://localhost:8080"
 
 type Options struct {
 	Addr    string
 	BaseURL string
 }
 
-var options Options
+var options = Options{
+	Addr:    ServerAddress,
+	BaseURL: BaseURL,
+}
+
+func setServerAddress(flagAddr string) string {
+	envAddr, ok := os.LookupEnv("SERVER_ADDRESS")
+
+	if ok {
+		return envAddr
+	}
+
+	return flagAddr
+}
+
+func setBaseURL(flagBaseURL string) string {
+	envBaseURL, ok := os.LookupEnv("BASE_URL")
+
+	if ok {
+		return envBaseURL
+	}
+
+	return flagBaseURL
+}
 
 func Init() Options {
-	flag.StringVar(&options.Addr, "a", "localhost:8080", "address and port to run server")
-	flag.StringVar(&options.BaseURL, "b", "http://localhost:8080", "base address of the resulting shortened URL")
+	flagAddr := flag.String("a", options.Addr, "address and port to run server")
+	flagBaseURL := flag.String("b", options.BaseURL, "base address of the resulting shortened URL")
 	flag.Parse()
+
+	options.Addr = setServerAddress(*flagAddr)
+	options.BaseURL = setBaseURL(*flagBaseURL)
 
 	return options
 }

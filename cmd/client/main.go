c5bfa35b08cd0dcb284283bffd351f04b1d285fe
tab <tukhfatullin@gmail.com>
2024-10-10 23:18:56 +0300

    refactor(client): enhance client configuration and add error handling

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -4,40 +4,67 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 
 	"github.com/go-resty/resty/v2"
 )
 
-type Options struct {
-	Endpoint string
+const (
+	BaseURL = "http://localhost:8080"
+)
+
+type Config struct {
+	BaseURL string
 }
 
-var opts Options
-
 func main() {
-	flag.StringVar(&opts.Endpoint, "a", "http://localhost:8080", "address and port to call server")
+	cfg := parseFlags()
+	longURL, err := readLongURL()
+	if err != nil {
+		log.Fatalf("Error reading URL: %v", err)
+	}
+
+	shortURL, err := createShortLink(cfg.BaseURL, longURL)
+	if err != nil {
+		log.Fatalf("Error creating short link: %v", err)
+	}
+
+	fmt.Printf("Short URL: %s\n", shortURL)
+}
+
+func parseFlags() Config {
+	var cfg Config
+	flag.StringVar(&cfg.BaseURL, "a", BaseURL, "address and port to call server")
 	flag.Parse()
+	return cfg
+}
 
+func readLongURL() (string, error) {
 	fmt.Println("Enter long URL:")
-
 	reader := bufio.NewReader(os.Stdin)
 	longURL, err := reader.ReadString('\n')
 	if err != nil {
-		panic(err)
+		return "", err
 	}
-	longURL = strings.TrimSuffix(longURL, "\n")
+	return strings.TrimSuffix(longURL, "\n"), nil
+}
 
+func createShortLink(endpoint, longURL string) (string, error) {
 	client := resty.New()
 	response, err := client.R().
 		SetHeader("Content-Type", "application/x-www-form-urlencoded").
 		SetBody(longURL).
-		Post(opts.Endpoint)
+		Post(endpoint)
+
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
-	fmt.Printf("Status code: %s\n", response.Status())
-	fmt.Printf("Short URL: %s\n", response.String())
+	if response.IsError() {
+		return "", fmt.Errorf("server responded with status code %d", response.StatusCode())
+	}
+
+	return response.String(), nil
 }

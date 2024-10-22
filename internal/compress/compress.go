37d350db7b34bfb18c01eeda2121eaf10c30363b
tab <tukhfatullin@gmail.com>
2024-10-22 22:15:10 +0300

    fix(compress): Update header setting condition in WriteHeader method
    Changed the condition to set 'Content-Encoding' to gzip to check for status codes between 100 and 226 inclusive.

--- internal/compress/compress.go
+++ internal/compress/compress.go
@@ -73,7 +73,7 @@ func (c *compressWriter) Write(p []byte) (int, error) {
 }
 
 func (c *compressWriter) WriteHeader(statusCode int) {
-	if statusCode < 300 {
+	if statusCode >= http.StatusContinue && statusCode <= http.StatusIMUsed {
 		c.w.Header().Set("Content-Encoding", "gzip")
 	}
 

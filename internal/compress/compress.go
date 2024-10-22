dbc07c32024e1b2407480768cb7a92735851a6da
tab <tukhfatullin@gmail.com>
2024-10-22 22:16:16 +0300

    refactor(compress): Rename variables for clarity in compress middleware
    Updated variable names in the compress middleware to improve code readability. 'ow' is now 'originalWriter', 'cr' is 'compressedReader', and 'zw' is 'gzipWriter'.

--- internal/compress/compress.go
+++ internal/compress/compress.go
@@ -9,84 +9,84 @@ import (
 
 func Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ow := w
+		originalWriter := w
 
 		if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
-			cr, err := newCompressReader(r.Body)
+			compressedReader, err := newCompressReader(r.Body)
 			if err != nil {
 				w.WriteHeader(http.StatusInternalServerError)
 				return
 			}
-			r.Body = cr
-			defer cr.Close()
+			r.Body = compressedReader
+			defer compressedReader.Close()
 		}
 
 		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
-			cw := newCompressWriter(w)
-			ow = cw
-			defer cw.Close()
+			compressedWriter := newCompressWriter(w)
+			originalWriter = compressedWriter
+			defer compressedWriter.Close()
 		}
 
-		next.ServeHTTP(ow, r)
+		next.ServeHTTP(originalWriter, r)
 	})
 }
 
 type compressReader struct {
-	r  io.ReadCloser
-	zr *gzip.Reader
+	reader     io.ReadCloser
+	gzipReader *gzip.Reader
 }
 
 func (c compressReader) Read(p []byte) (n int, err error) {
-	return c.zr.Read(p)
+	return c.gzipReader.Read(p)
 }
 
 func (c *compressReader) Close() error {
-	if err := c.r.Close(); err != nil {
+	if err := c.reader.Close(); err != nil {
 		return err
 	}
-	return c.zr.Close()
+	return c.gzipReader.Close()
 }
 
-func newCompressReader(r io.ReadCloser) (*compressReader, error) {
-	zr, err := gzip.NewReader(r)
+func newCompressReader(reader io.ReadCloser) (*compressReader, error) {
+	gzipReader, err := gzip.NewReader(reader)
 	if err != nil {
 		return nil, err
 	}
 
 	return &compressReader{
-		r:  r,
-		zr: zr,
+		reader:     reader,
+		gzipReader: gzipReader,
 	}, nil
 }
 
 type compressWriter struct {
-	w  http.ResponseWriter
-	zw *gzip.Writer
+	writer     http.ResponseWriter
+	gzipWriter *gzip.Writer
 }
 
 func (c *compressWriter) Header() http.Header {
-	return c.w.Header()
+	return c.writer.Header()
 }
 
 func (c *compressWriter) Write(p []byte) (int, error) {
-	return c.zw.Write(p)
+	return c.gzipWriter.Write(p)
 }
 
 func (c *compressWriter) WriteHeader(statusCode int) {
 	if statusCode >= http.StatusContinue && statusCode <= http.StatusIMUsed {
-		c.w.Header().Set("Content-Encoding", "gzip")
+		c.writer.Header().Set("Content-Encoding", "gzip")
 	}
 
-	c.w.WriteHeader(statusCode)
+	c.writer.WriteHeader(statusCode)
 }
 
 func (c *compressWriter) Close() error {
-	return c.zw.Close()
+	return c.gzipWriter.Close()
 }
 
-func newCompressWriter(w http.ResponseWriter) *compressWriter {
+func newCompressWriter(writer http.ResponseWriter) *compressWriter {
 	return &compressWriter{
-		w:  w,
-		zw: gzip.NewWriter(w),
+		writer:     writer,
+		gzipWriter: gzip.NewWriter(writer),
 	}
 }

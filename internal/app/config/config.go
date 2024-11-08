126e226718f24192d734a01996c7303eb1fb5bd6
tab <tukhfatullin@gmail.com>
2024-11-08 22:26:58 +0200

    feat(config): Add environment configuration support with dotenv
    Introduced .env files for development and testing environments. Updated config loading to support environment variables from these files.

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -2,17 +2,16 @@ package config
 
 import (
 	"flag"
+	"fmt"
 	"os"
-)
 
-const (
-	BaseURL         = "http://localhost:8080"
-	ClientURL       = "http://localhost:3000"
-	ServerAddress   = "localhost:8080"
-	FileStoragePath = "store.json"
+	"github.com/joho/godotenv"
+
+	"shortly/internal/logger"
 )
 
 type Config struct {
+	AppEnv          string
 	Addr            string
 	BaseURL         string
 	ClientURL       string
@@ -20,13 +19,34 @@ type Config struct {
 }
 
 func LoadConfig() *Config {
-	flagAddr := flag.String("a", ServerAddress, "address and port to run server")
-	flagBaseURL := flag.String("b", BaseURL, "base address of the resulting shortened URL")
-	flagClientURL := flag.String("c", ClientURL, "frontend client URL")
-	flagFileStoragePath := flag.String("f", FileStoragePath, "path to the file storage")
+	appLogger := logger.NewLogger()
+
+	env := os.Getenv("GO_ENV")
+	if env == "" {
+		env = "development"
+	}
+
+	envFiles := []string{
+		".env",
+		fmt.Sprintf(".env.%s", env),
+		fmt.Sprintf(".env.%s.local", env),
+	}
+	for _, file := range envFiles {
+		err := godotenv.Overload(file)
+		if err == nil {
+			appLogger.Info().Msgf("Loaded %s file", file)
+		}
+	}
+
+	flagAddr := flag.String("a", "", "address and port to run server")
+	flagBaseURL := flag.String("b", "", "base address of the resulting shortened URL")
+	flagClientURL := flag.String("c", "", "frontend client URL")
+	flagFileStoragePath := flag.String("f", "", "path to the file storage")
+	flagDatabaseDSN := flag.String("d", "", "database DSN")
 	flag.Parse()
 
 	return &Config{
+		AppEnv:          env,
 		Addr:            getEnvOrFlag("SERVER_ADDRESS", *flagAddr),
 		BaseURL:         getEnvOrFlag("BASE_URL", *flagBaseURL),
 		ClientURL:       getEnvOrFlag("CLIENT_URL", *flagClientURL),
@@ -35,7 +55,7 @@ func LoadConfig() *Config {
 }
 
 func getEnvOrFlag(envVar, flagValue string) string {
-	if envValue, ok := os.LookupEnv(envVar); ok {
+	if envValue, ok := os.LookupEnv(envVar); ok && envValue != "" {
 		return envValue
 	}
 	return flagValue
--- internal/app/config/config_test.go
+++ internal/app/config/config_test.go
@@ -6,7 +6,18 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"shortly/internal/spec"
 )
+
+func TestMain(m *testing.M) {
+	if err := spec.LoadEnv(); err != nil {
+		log.Fatalf("Error loading environment variables: %v", err)
+	}
+
+	code := m.Run()
+	os.Exit(code)
+}
 
 func Test_LoadConfig(t *testing.T) {
 	tests := []struct {
@@ -20,15 +31,21 @@ func Test_LoadConfig(t *testing.T) {
 			args: []string{},
 			env:  map[string]string{},
 			expected: &Config{
-				Addr:            ServerAddress,
-				BaseURL:         BaseURL,
-				ClientURL:       ClientURL,
-				FileStoragePath: FileStoragePath,
+				AppEnv:          "test",
+				Addr:            "localhost:8080",
+				BaseURL:         "http://localhost:8080",
+				ClientURL:       "http://localhost:3000",
+				FileStoragePath: "store-test.json",
 			},
 		},
 		{
 			name: "Use env vars",
-			args: []string{"-a", "localhost:5000", "-b", "http://localhost:5000", "-c", "http://localhost:6000"},
+			args: []string{
+				"-a", "localhost:5000",
+				"-b", "http://localhost:5000",
+				"-c", "http://localhost:6000",
+				"-f", "store-test.json",
+			},
 			env: map[string]string{
 				"SERVER_ADDRESS":    "localhost:3000",
 				"BASE_URL":          "http://localhost:3000",
@@ -36,6 +53,7 @@ func Test_LoadConfig(t *testing.T) {
 				"FILE_STORAGE_PATH": "store-test.json",
 			},
 			expected: &Config{
+				AppEnv:          "test",
 				Addr:            "localhost:3000",
 				BaseURL:         "http://localhost:3000",
 				ClientURL:       "http://localhost:6000",
@@ -53,9 +71,11 @@ func Test_LoadConfig(t *testing.T) {
 			flag.CommandLine = flag.NewFlagSet(tt.name, flag.ContinueOnError)
 			result := LoadConfig()
 
+			assert.Equal(t, tt.expected.AppEnv, result.AppEnv)
 			assert.Equal(t, tt.expected.Addr, result.Addr)
 			assert.Equal(t, tt.expected.BaseURL, result.BaseURL)
 			assert.Equal(t, tt.expected.ClientURL, result.ClientURL)
+			assert.Equal(t, tt.expected.FileStoragePath, result.FileStoragePath)
 
 			t.Cleanup(func() {
 				for key := range tt.env {

071dad66ecc6d68e404055719ea757ed62774184
tab <tukhfatullin@gmail.com>
2025-02-07 18:31:08 +0200

    feat(staticlint): Add new analyzers for static analysis
    Added additional analyzers: assign, httpresponse, loopclosure, lostcancel, and unsafeptr

--- cmd/staticlint/main.go
+++ cmd/staticlint/main.go
@@ -3,17 +3,27 @@ package main
 import (
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/multichecker"
+	"golang.org/x/tools/go/analysis/passes/assign"
+	"golang.org/x/tools/go/analysis/passes/httpresponse"
+	"golang.org/x/tools/go/analysis/passes/loopclosure"
+	"golang.org/x/tools/go/analysis/passes/lostcancel"
 	"golang.org/x/tools/go/analysis/passes/printf"
 	"golang.org/x/tools/go/analysis/passes/shadow"
 	"golang.org/x/tools/go/analysis/passes/structtag"
+	"golang.org/x/tools/go/analysis/passes/unsafeptr"
 	"honnef.co/go/tools/staticcheck"
 )
 
 func main() {
 	analyzers := []*analysis.Analyzer{
+		assign.Analyzer,
+		httpresponse.Analyzer,
+		loopclosure.Analyzer,
+		lostcancel.Analyzer,
 		printf.Analyzer,
 		shadow.Analyzer,
 		structtag.Analyzer,
+		unsafeptr.Analyzer,
 	}
 
 	for _, analyzer := range staticcheck.Analyzers {

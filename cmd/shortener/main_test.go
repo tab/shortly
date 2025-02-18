e32429c3b6fbf2192fad9eaf68bd20a86aeb7683
tab <tukhfatullin@gmail.com>
2025-02-18 13:02:38 +0200

    feat(app): Enhance graceful shutdown
    Updated the main function to handle SIGTERM, SIGINT, and SIGQUIT signals

--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -1,35 +1,62 @@
 package main
 
 import (
+	"flag"
 	"os"
+	"syscall"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 )
 
 func Test_Main(t *testing.T) {
-	oldArgs := os.Args
-	defer func() { os.Args = oldArgs }()
-
-	os.Args = []string{oldArgs[0], "-a=localhost:0"}
-
-	done := make(chan struct{})
-
-	go func() {
-		main()
-		close(done)
-	}()
-
-	time.Sleep(100 * time.Millisecond)
-
-	p, err := os.FindProcess(os.Getpid())
-	if err == nil && p != nil {
-		_ = p.Signal(os.Interrupt)
+	tests := []struct {
+		name   string
+		before func()
+		signal os.Signal
+	}{
+		{
+			name:   "SIGTERM",
+			signal: syscall.SIGTERM,
+		},
+		{
+			name:   "SIGINT",
+			signal: syscall.SIGINT,
+		},
+		{
+			name:   "SIGQUIT",
+			signal: syscall.SIGQUIT,
+		},
 	}
 
-	select {
-	case <-done:
-		// main() exited successfully
-	case <-time.After(1 * time.Second):
-		t.Fatal("timeout: main() did not exit after signal")
+	for _, tt := range tests {
+		oldArgs := os.Args
+		defer func() { os.Args = oldArgs }()
+		os.Args = []string{oldArgs[0], "-a=localhost:0"}
+
+		flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+
+		done := make(chan struct{})
+
+		go func() {
+			main()
+			close(done)
+		}()
+
+		time.Sleep(100 * time.Millisecond)
+
+		p, err := os.FindProcess(os.Getpid())
+		require.NoError(t, err)
+		require.NotNil(t, p)
+
+		require.NoError(t, p.Signal(tt.signal))
+
+		select {
+		case <-done:
+			// main() exited successfully
+		case <-time.After(1 * time.Second):
+			t.Fatal("timeout: main() did not exit after signal")
+		}
 	}
 }
